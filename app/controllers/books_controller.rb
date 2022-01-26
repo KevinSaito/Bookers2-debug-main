1ce0cf8e13e797ccdd5b4fffaec19fc6a68862b9
EC2 Default User <u6cfdm3p@gmail.com>
2022-01-26 04:06:57 +0000

    [Finish]解説通りに変更してエラーなし確認

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,8 @@ class BooksController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def show
-    @findbook = Book.find(params[:id])
-    @book = Book.new
+    @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
