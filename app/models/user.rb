1050ab6e4ecf572515c94233a9b7b51eb31f7df0
EC2 Default User <u6cfdm3p@gmail.com>
2022-01-29 12:28:14 +0000

    [Update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,25 @@ class User < ApplicationRecord
   has_many :_comments, dependent: :destroy
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length:{maximum:50}
 
 
-
+  def follow(user_id)
+    follower_create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    following_user.include?(user)
+  end
+  
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
     
     resources :book_comments, only:[:create,:destroy]
   end
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
