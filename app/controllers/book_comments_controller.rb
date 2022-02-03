5552ef04a8bb305c4abb0962ed8a0de1eade7da1
EC2 Default User <u6cfdm3p@gmail.com>
2022-02-03 12:25:22 +0000

    [Update]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,10 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.new(book_comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = book.id
+    @comment.save
     redirect_to request.referer
   end
   
